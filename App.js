c08d0e798b128d1feabb712f14689c62be5bb61f
syamn132 <ssrn132@gmail.com>
2023-10-31 14:43:47 +0530

    Added Button

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
 
 export default function App() {
+  const [isContentVisible, setContentVisible] = useState(false);
+  const [message, setMessage] = useState(' ');
+  const [imageSource, setImageSource] = useState('https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png');
+  const [buttonText, setButtonText] = useState('Show');
+  const [buttonBackgroundColor, setButtonBackgroundColor] = useState('#22c55e');
+  const [counter, setCounter] = useState(0);
+
+  const toggleContentVisibility = () => {
+    const prevMessage = message;
+    if (prevMessage === ' ') {
+      setMessage('Hello World');
+      setImageSource('https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png');
+      setButtonText('Hide');
+      setButtonBackgroundColor('#e12d39');
+      setContentVisible(true)
+    } else {
+      setMessage(' ');
+      setImageSource('https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png');
+      setButtonText('Show');
+      setButtonBackgroundColor('#22c55e');
+    }
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.darkBackground, { backgroundColor: '#0b0b0b' }]}>
+        <View>
+          <Image source={{ uri: imageSource }} style={styles.bulbImage} />
+        </View>
+        {isContentVisible && (
+          <View>
+            <Text style={styles.textMsg}>{message}</Text>
+          </View>
+        )}
+        <Text style={styles.cntCss}>Count : {counter}</Text>
+        <View>
+          <Button 
+            title={buttonText}
+            style = {styles.button}
+            color={buttonBackgroundColor}
+            onPress={toggleContentVisibility} 
+            />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +54,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0b0b0b'
+  },
+  darkBackground: {
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  bulbImage: {
+    flex: .7,
+    width: 160,
+    resizeMode: 'contain'
+  },
+  textMsg: {
+    color: '#e3ca2d',
+    fontSize: 40,
+  },
+  cntCss: {
+    color: 'grey',
+    fontSize: 20,
+    marginBottom: 10
   },
 });
